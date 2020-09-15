e27911e2a3853eda0a6d1e9f26ac847be883f580
Lee12312 <akak640@kyonggi.ac.kr>
2020-09-15 17:58:21 +0900

    exercise_0915

--- python/test.py
+++ python/test.py
@@ -1,13 +1,5 @@
-#한 번 계산된 결과를 메모이제이션 하기위한 리스트 초기화
-d = [0] * 100
+dic = {'hello': 1, 'hi' : 2, 'hihello' : 3}
 
-#첫 번째 피보나치 수와 두 번째 피보나치 수는 1
-d[0] = 1
-d[1] = 1
-n = 99
-
-#피보나치 함수 반복문으로 구현
-for i in range(3, n+1):
-    d[i] = d[i-1] + d[i-2]
-
-print(d[n])
+print(dic['hi'])
+dic['dongdang'] = 4
+print(dic.items())
