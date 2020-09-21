8be11b8e86f81687bed170e4812d0be78ad9503e
Lee12312 <akak640@kyonggi.ac.kr>
2020-09-21 18:53:59 +0900

    exercise_0921

--- python/test1.py
+++ python/test1.py
@@ -1,14 +1,20 @@
-#임의의 수  입력받기
-n,k = map(int, input().split())
+#공간의 크기 입력받기
+n = int(input())
+#여행가가 이동할 계획서 내용을 입력받기
+plan_data = list(map(str, input().split()))
 
-d = [0]*(n+1)
-d[0] = 0
-d[1] = 0
+start = (0,0)
+#이동 반경 리스트 만들기
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
-for i in range(2,n+1):
-    d[i] = d[i-1] + 1
+for i in range(len(plan_data)):
+    if plan_data[i] == 'U':
 
-    if i % k == 0:
-        d[i] = min(d[i], d[i//k] + 1)
+    elif plan_data[i] == 'D':
 
-print(d[n])
+    elif plan_data[i] == 'L':
+
+    elif plan_data[i] == 'R':
+
+    
