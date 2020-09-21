f0ee55b746861968a96e11242f2e078f94b1e062
Lee12312 <akak640@kyonggi.ac.kr>
2020-09-21 18:43:43 +0900

    exercise_0921

--- python/test1.py
+++ python/test1.py
@@ -1,8 +1,14 @@
-import heapq
+#임의의 수  입력받기
+n,k = map(int, input().split())
 
-q = []
+d = [0]*(n+1)
+d[0] = 0
+d[1] = 0
 
-heapq.heappush(q, (1,2))
-heapq.heappush(q, (0,1))
-print(heapq.heappop(q))
-print(heapq.heappop(q))
+for i in range(2,n+1):
+    d[i] = d[i-1] + 1
+
+    if i % k == 0:
+        d[i] = min(d[i], d[i//k] + 1)
+
+print(d[n])
