79254c78953afd16688e25de31069089ee2651ba
Lee12312 <akak640@kyonggi.ac.kr>
2020-09-30 17:16:34 +0900

    algorithmexercise_0930

--- python/Luckystraight.py
+++ python/Luckystraight.py
@@ -1,20 +1,22 @@
 #p.321 럭키 스트레이트
 
 #점수 입력받기
-n = list(map(int, input().split()))
+n = list(map(int, input()))
 #중간지점구하기
 first = 0
 end = len(n)
 mid = (first+end)//2
 
-sum_left = 0
-sum_right = 0
-for i in range(first,mid):
-    sum_left += n[i]
-for j in range(mid,end):
-    sum_right += n[j]
+sum_left = []
+sum_right = []
 
-if sum_left == sum_right:
+for i in range(len(n)):
+    if i < mid:
+        sum_left.append(n[i])
+    else:
+        sum_right.append(n[i])
+
+if sum(sum_left) == sum(sum_right):
     print('LUCKY')
 else:
     print('READY')
